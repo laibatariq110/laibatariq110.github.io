a05d1c316f44febdb63c21700af5e4e529378272
laibatariq110 <laibatariq110@gmail.com>
2024-12-09 21:18:46 +0500

    fixed error

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.scss'; 
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/laibatariq110.github.io/', 
   plugins: [react()],
-  
+  build: {
+    outDir: 'dist', 
+    assetsDir: 'assets', 
+  },
 });
